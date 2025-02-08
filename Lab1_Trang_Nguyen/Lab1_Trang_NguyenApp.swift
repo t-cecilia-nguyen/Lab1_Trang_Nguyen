4345463c95bd3fb7889499c0916f2e71f5d3ca7d
Trang Nguyen <w5749684@macmini27.gbccloud.com>
2025-02-08 16:13:28 -0500

    initial commit

---
+++ Lab1_Trang_Nguyen/Lab1_Trang_NguyenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Trang_NguyenApp.swift
+//  Lab1_Trang_Nguyen
+//
+//  Created by Trang Nguyen on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Trang_NguyenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
