804aedd5d2da92f41acc79dccb54ea6cc5f1e3cc
Trang Nguyen <w5749684@macmini27.gbccloud.com>
2025-02-08 16:39:58 -0500

    created labels

--- Lab1_Trang_Nguyen/ContentView.swift
+++ Lab1_Trang_Nguyen/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentNumber: Int = Int.random(in: 1...1000)
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(currentNumber)")
+                .font(.system(size: 60, weight: .bold))
+            VStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Prime")
+                        .frame(width: 100, height: 100)
+                        .font(.system(size: 40))
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Not Prime")
+                        .frame(width: 200, height: 100)
+                        .font(.system(size: 40))
+                }
+            }
         }
         .padding()
     }
