c1589ef55aaa1916b1f934ae31f16a9eea75a7ae
Trang Nguyen <w5749684@macmini30.gbccloud.com>
2025-02-08 19:55:34 -0500

    fixed layout/logic

--- Lab1_Trang_Nguyen/ContentView.swift
+++ Lab1_Trang_Nguyen/ContentView.swift
@@ -12,29 +12,31 @@ struct ContentView: View {
     @State private var showResponse: Bool = false
     @State private var isCorrect: Bool = false
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("\(currentNumber)")
                 .font(.system(size: 60, weight: .bold))
-            VStack {
-                Button(action:  { checkAnswer(isPrimeSelected: true) }) {
-                    Text("Prime")
-                        .frame(width: 100, height: 100)
-                        .font(.system(size: 40))
-                }
-                Button(action: { checkAnswer(isPrimeSelected: false) }) {
-                    Text("Not Prime")
-                        .frame(width: 200, height: 100)
-                        .font(.system(size: 40))
-                }
+
+            Button(action:  { checkAnswer(isPrimeSelected: true) }) {
+                Text("Prime")
+                    .font(.system(size: 40))
+            }
+            
+            Button(action: { checkAnswer(isPrimeSelected: false) }) {
+                Text("Not Prime")
+                    .font(.system(size: 40))
+            }
+
+            
+            if showResponse {
+                Text(isCorrect ? "Correct" : "Wrong")
+                    .font(.system(size: 40))
+            } else {
+                Text(" ")
+                    .font(.system(size: 40))
             }
         }
         .padding()
-        
-        if showResponse {
-            Text(isCorrect ? "Correct" : "Wrong")
-                .font(.system(size: 50))
-        }
-        
+
     }
     // Check if prime
     func isPrime(_ number: Int) -> Bool {
